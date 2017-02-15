b61884366db83c9d7f0c938218482ae6d6ea697e
Anuj More <anujmorex@gmail.com>
2017-02-15 02:07:31 -0600

    Uncommentify

--- lib/merzu/transliteration.rb
+++ lib/merzu/transliteration.rb
@@ -1,89 +1,49 @@
 class Merzu::Transliteration
-  # ::valid_scripts
-  #
   # List of valid scripts
-  # [:iast, :devanagari, :bengali, :gurmukhi, :gujarati, :oriya, :tamil,
-  # :telugu, :kannada, :malayalam, :singhalese]
+  # [:iast, :devanagari, :bengali, :gurmukhi, :gujarati, :oriya, :tamil, :telugu, :kannada, :malayalam, :singhalese]
   def self.valid_scripts
-    Merzu::Data.mapping_headers
+    Merzu::Data.mapping_headers.map(&:downcase).map(&:to_sym)
   end
 
-  # ::new(text, source_script)
-  #
-  # Initialize the Transliteration object
-  #   eg. Merzu::Transliteration.new("कशी आहेस?", :devanagari)
   def initialize(text, source_script)
     @text = text
     @source_script = source_script
   end
 
-  # #transliterate(target_script)
-  #
-  # Transliterate from one script to another
-  #   eg. transliterate_to(:malayalam)
-  #
-  # The mappings are defined with respect to IAST. So a mapping like Devanagari
-  # to Malayalam will be done as follows:
-  #
-  #   Devanagari -> IAST -> Malayalam
-  #
-  # Additional rules can be passed in opts. Passed rules hold precedence over
-  # the prebuilt rules.
   def transliterate_to(target_script, opts={})
     # TODO: Raise some sensible error if target_script is not in valid scripts
     raise NotImplementedError unless self.class.valid_scripts.include? target_script
 
-    if target_script == :iast
-      # If target script is IAST, use the private method to transliterate to
-      # IAST
-      return transliterate_to_iast
+    return transliterate_to_iast if target_script == :iast
+    return transliterate_from_iast_to(target_script) if source_script == :iast
 
-    elsif source_script == :iast
-      # If source script is IAST, use the private method to transliterate to the
-      # input method
-      return transliterate_from_iast_to target_script
-
-    else
-      # If none of the source or target script is :iast, get the IAST
-      # representation of the source text and feed that representation to a new
-      # Transliteration object, and call transliterate_to on it with the target
-      # script
-      iast_str = transliterate_to :iast
-      iast_object = Merzu::Transliteration.new(iast_str, :iast)
-      return iast_object.transliterate_to(target_script)
-    end
+    iast_str = transliterate_to :iast
+    iast_object = Merzu::Transliteration.new(iast_str, :iast)
+    return iast_object.transliterate_to(target_script)
   end
 
   private
-
-  # #text
-  # #source_script
-  #
-  # Make the getters `text` and `source_script` available in the class
   attr_reader :text, :source_script
 
-  # #transliterate_to_iast
-  #
-  # Transliterate the text in source_script to IAST
   def transliterate_to_iast
-    ""
+    iast_index = self.class.valid_scripts.index(:iast)
+    iast_array = fetch_mapping_array(iast_index)
+    source_index = self.class.valid_scripts.index(source_script)
+    source_array = fetch_mapping_array(source_index)
+
+    binding.pry
   end
 
-  # #transliterate_from_iast_to
-  #
-  # Transliterate the current IAST source text to the given target script.
-  #
-  # Exception conditions:
-  #   Raise an error if the source_script is not IAST
   def transliterate_from_iast_to(target_script)
     # TODO: Raise some sensible error if source_script is not "IAST"
     raise NotImplementedError if source_script != :iast
     ""
   end
 
-  # #valid
-  #
-  # At any point, return if the state of the Transliteration object is valid
+  def fetch_mapping_array(index)
+    Merzu::Data.mapping.map { |line| line[index] }
+  end
+
   def valid?
     return true if text && self.class.valid_scripts.include?(source_script)
     false
